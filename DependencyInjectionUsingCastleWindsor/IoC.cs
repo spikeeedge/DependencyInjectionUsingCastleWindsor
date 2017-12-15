119ec7332e8505b7f8f3eb36f466175f83eddf48
Bijal <bjlthakkar@gmail.com>
2017-12-15 13:36:59 +1030

    Added IoC Registration Support

---
+++ DependencyInjectionUsingCastleWindsor/IoC.cs
@@ -0,0 +1,20 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace DependencyInjectionUsingCastleWindsor
+{
+    public class IoC
+    {
+        public static void Register()
+        {
+            var container = new WindsorContainer();
+
+            container.Register(
+                Classes.FromThisAssembly()
+                .InSameNamespaceAs<Program>(true)
+                .WithService
+                .DefaultInterfaces()
+                .LifestyleTransient());
+        }
+    }
+}
--- DependencyInjectionUsingCastleWindsor/Program.cs
+++ DependencyInjectionUsingCastleWindsor/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DependencyInjectionUsingCastleWindsor
+﻿namespace DependencyInjectionUsingCastleWindsor
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Program Started");
+
+            IoC.Register();
+
+            Console.WriteLine("IoC Registered");
         }
     }
 }
